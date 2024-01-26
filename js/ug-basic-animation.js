1cc5875c30bffdc1bfb225e14880ac622a9f6fac
Eugene <eugenermondreal@gmail.com>
2024-01-27 04:38:05 +0800

    fixed animate condition

--- js/ug-basic-animation.js
+++ js/ug-basic-animation.js
@@ -1,25 +1,32 @@
 var scrollLoad = function () {
     var animateElements = document.querySelectorAll('.ug-animate');
+
     if (animateElements.length > 0) {
         animateElements.forEach(function (element) {
             var wH = window.innerHeight;
             var sT = window.scrollY;
             var oT = element.offsetTop;
-            var s = wH + sT - 10;
-            var fadeInUpAnim = element.getAttribute('data-animation') === 'fadeInUp';
-            var fadeInDownAnim = element.getAttribute('data-animation') === 'fadeInDown';
-            var fadeInLeftAnim = element.getAttribute('data-animation') === 'fadeInLeft';
-            var fadeInRightAnim = element.getAttribute('data-animation') === 'fadeInRight';
-
-            if (s > oT && fadeInUpAnim) {
-                element.classList.add('fadeInUp');
-            } else if (s > oT && fadeInDownAnim) {
-                element.classList.add('fadeInDown');
-            } else if (s > oT && fadeInLeftAnim) {
-                element.classList.add('fadeInLeft');
-            } else if (s > oT && fadeInRightAnim) {
-                element.classList.add('fadeInRight');
+            var tH = element.clientHeight;
+            var animationType = element.getAttribute('data-animation');
+            // Adjust condition to check if at least half of the element is visible in the window
+            if (sT + wH > oT + tH * 0.5) {
+                switch (animationType) {
+                    case 'fadeInUp':
+                        element.classList.add('fadeInUp');
+                        break;
+                    case 'fadeInDown':
+                        element.classList.add('fadeInDown');
+                        break;
+                    case 'fadeInLeft':
+                        element.classList.add('fadeInLeft');
+                        break;
+                    case 'fadeInRight':
+                        element.classList.add('fadeInRight');
+                        break;
+                }
             }
+
+            console.log(`height: ${wH}\nscroll: ${sT}\nscroll: ${oT}`);
         });
     }
 };
